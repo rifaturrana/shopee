de53bd17c1d75ac1d58c0e73061a6c8935d9853e
rifaturrana <smrifaturrana@gmail.com>
2023-05-09 14:50:02 +0600

    Product by id and View Profile added

--- Product/urls.py
+++ Product/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
 
        path('', all_products, name='all_products'),
+       path('<int:product_id>/', product_details, name='product_details'),
 ]
--- Product/views.py
+++ Product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import *
 # Create your views here.
 
@@ -8,4 +8,13 @@ def all_products(request):
     return render(request,'product/all_products.html',{
         'products':products,
         'categories':categories
+    })
+
+def product_details(request,product_id):
+    try:
+      product=Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return HttpResponse("Product Not Found")
+    return render(request,'product/product_details.html',{
+        'product':product
     })
